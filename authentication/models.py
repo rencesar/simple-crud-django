23b7e25abb18dd1a227b74ce9d7f8fdc84b8139c
Renato César <rencesarlira@gmail.com>
2019-07-07 15:37:03 -0300

    username add

--- authentication/models.py
+++ authentication/models.py
@@ -7,9 +7,9 @@ from django.utils.translation import gettext as _
 class User(AbstractUser):
     name = models.CharField(_('Name'), max_length=120, blank=True)
     email = models.EmailField(_('email address'), blank=False, null=False, unique=True)
+    username = models.CharField(_('username'), max_length=150)
     first_name = None
     last_name = None
-    username = None
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
