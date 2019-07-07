bc92307320ec8b0e2919d8f7c74798315e5fa35c
Renato César <rencesarlira@gmail.com>
2019-07-07 07:39:31 -0300

    Refactoring base template and add list functionality

--- clients/views.py
+++ clients/views.py
@@ -2,10 +2,12 @@ from django.views import generic
 from django.urls import reverse_lazy
 
 from .forms import ClientForm
+from .models import Clients
 
 
-class ClientListView(generic.TemplateView):
+class ClientListView(generic.ListView):
     template_name = 'list-clients.html'
+    model = Clients
 
 
 class ClientCreateView(generic.CreateView):
--- servicenet/context_processor.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-
-def base_context_processors(request):
-    return {'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY}
